b313248e79cbfba5fbc562d3d9c1faa207e4cafd
Ahmed-469 <ahmed3almanoo@gmail.com>
2026-05-11 20:27:27 +0300

    game-model

---
+++ models/Game.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const gameSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    gameUrl: {
+      type: String,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Game = mongoose.model("Game", gameSchema)
+
+export default Game
